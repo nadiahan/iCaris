02837f66012d28e0ae4bcddb2a585cde736f21c0
nadiahan <nadiahanz00@gmail.com>
2022-01-08 15:42:24 +0800

    Update svlt3

--- src/java/svlt3.java
+++ src/java/svlt3.java
@@ -1,47 +1,53 @@
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 @WebServlet(urlPatterns = {"/svlt3"})
 public class svlt3 extends HttpServlet {
 
-    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
         try (PrintWriter out = response.getWriter()) {
-            /* TODO output your page here. You may use following sample code. */
-            out.println("<!DOCTYPE html>");
-            out.println("<html>");
-            out.println("<head>");
-            out.println("<title>Servlet svlt3</title>");            
-            out.println("</head>");
-            out.println("<body>");
-            out.println("<h1>Servlet svlt3 at " + request.getContextPath() + "</h1>");
-            out.println("</body>");
-            out.println("</html>");
+           //fetch data
+            HttpSession session = request.getSession();
+            int bookingID = Integer.parseInt(session.getAttribute("bookingID").toString());
+            
+            //work on database part
+            String driver = "com.mysql.jdbc.Driver";
+            String connectionUrl = "jdbc:mysql://localhost:3306/";
+            String database = "rentalproject";
+            String userid = "root";
+            String password = "";
+            
+            try {
+                Class.forName(driver);
+                Connection conn = DriverManager.getConnection(connectionUrl+database,userid,password);
+               
+                //prepared statement 
+                
+                String sqlupdate = "delete from booking where id=?";
+                PreparedStatement ps = conn.prepareStatement(sqlupdate);
+                ps.setInt(1, bookingID);
+                ps.executeUpdate();
+                
+                log(sqlupdate);
+            
+                response.sendRedirect("booking_list.jsp");
+            }
+            
+            catch(Exception ex){
+                ex.printStackTrace();
+            }
         }
     }
-
-    @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        processRequest(request, response);
-    }
-
-    @Override
-    protected void doPost(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        processRequest(request, response);
-    }
-
-    @Override
-    public String getServletInfo() {
-        return "Short description";
-    }// </editor-fold>
-
 }
